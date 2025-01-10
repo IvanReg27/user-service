fbe50931578f4fd2b15a52212bece67467029e50
IvanIvanReg27 <makhorin0088@gmail.com>
2025-01-10 14:09:03 +0300

    создание БД, сущности и миграции для User v04

--- src/main/java/com/aston/userservice/repository/UserRepository.java
+++ src/main/java/com/aston/userservice/repository/UserRepository.java
@@ -9,9 +9,17 @@ import java.util.UUID;
 
 /**
  * Интерфейс репозитория для работы с пользователем
+ *
  * @see User
  */
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    /**
+     * Метод для получения пользователя по login
+     *
+     * @param login login карты
+     * @return пользователь с заданным login
+     */
     Optional<User> findByLogin(String login);
 }
