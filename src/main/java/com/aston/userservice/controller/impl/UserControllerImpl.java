003b183568e1d1fa4ed7c94522f8d5faa2dc5b3c
IvanIvanReg27 <makhorin0088@gmail.com>
2025-01-15 14:35:57 +0300

    cоздание рест эндпоинта v05

--- src/main/java/com/aston/userservice/controller/impl/UserControllerImpl.java
+++ src/main/java/com/aston/userservice/controller/impl/UserControllerImpl.java
@@ -3,21 +3,20 @@ package com.aston.userservice.controller.impl;
 import com.aston.userservice.controller.UserController;
 import com.aston.userservice.domain.response.UserRequisitesResponseDTO;
 import com.aston.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
 @RestController
+@RequiredArgsConstructor
 public class UserControllerImpl implements UserController {
 
     private UserService userService;
 
-    public UserControllerImpl(UserService userService) {
-    }
-
     @Override
     public ResponseEntity<UserRequisitesResponseDTO> getUserRequisites(UUID id) {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(userService.getUserRequisitesById(id));
     }
 }
