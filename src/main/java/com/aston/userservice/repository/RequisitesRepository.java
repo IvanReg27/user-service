2402cf51b89e414a20e075d109ba8526ecc46eb4
IvanIvanReg27 <makhorin0088@gmail.com>
2025-01-22 08:50:38 +0300

    cоздание рест эндпоинта v18

--- src/main/java/com/aston/userservice/repository/RequisitesRepository.java
+++ src/main/java/com/aston/userservice/repository/RequisitesRepository.java
@@ -24,7 +24,7 @@ public interface RequisitesRepository extends JpaRepository<Requisites, UUID> {
      * @param userId userId пользователя
      * @return пользователь с заданным id
      */
-    @Query("select u.firstName as firstName, TRIM(r.accountNumber) as accountNumber, TRIM(r.kbk) as kbk " +
+    @Query("select u.firstName as firstName, trim(r.accountNumber) as accountNumber, trim(r.kbk) as kbk " +
             "from Requisites r join r.user u where u.id = :userId")
     Optional<UserRequisitesProjection> findByUserId(@Param("userId") UUID userId);
 }
