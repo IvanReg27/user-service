e27a0395c2c9e9755a6d376982b9c0f027c90176
IvanIvanReg27 <makhorin0088@gmail.com>
2025-03-27 18:37:26 +0300

    написал конфигурацию для редис коммит_05

---
+++ src/main/java/com/aston/userservice/config/redis/CacheConfig.java
@@ -0,0 +1,27 @@
+package com.aston.userservice.config.redis;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(30)) // Время жизни кеша (30 минут)
+                .disableCachingNullValues(); // Не кешировать null-значения
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfiguration)
+                .build();
+    }
+}
